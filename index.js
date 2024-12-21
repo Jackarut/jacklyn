313f01c386a5cfd5472d68d5243326670f039910
Jackarut <turajack97@gmail.com>
2024-12-21 18:23:31 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+fetch('https://fakestoreapi.com/products')
+.then(response => response.json())
+.then(products => {
+  // Loop through the products and create HTML cards
+  products.forEach(product => {
+    const productCard = document.createElement('div');
+    productCard.className = 'product-card';
+
+    productCard.innerHTML = `
+      <img src="${product.image}" alt="${product.title}">
+      <div class="product-title">${product.title}</div>
+      <div class="product-price">$${product.price.toFixed(2)}</div>
+      <div class="product-description">${product.description}</div>
+    `;
+
+    productGrid.appendChild(productCard);
+  });
+})
+.catch(error => {
+  console.error('Error fetching products:', error);
+});
+
+// Event listener for the button
+fetchButton.addEventListener('click', fetchDogImage);
+
+// Fetch an initial image when the page loads
+fetchDogImage();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+   navigator.serviceWorker.register('../sw.js').then( () => {
+    console.log('Service Worker Registered')
+   })
+ })
+}
+
+
+
